e9b5492997a1d83e76a81b5803d034fb4b0a053e
Daefron <thomas_evans@outlook.com>
2024-12-23 14:02:05 +1100

    added scroll error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,85 @@
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 export default function App() {
   const [scrollPoint, setScrollPoint] = useState(0);
-  const tabOrder = ["about", "project1", "project2", "contact"];
+  const scrollRef = useRef();
+  scrollRef.current = scrollPoint;
   const scrollDelay = useRef(false);
-  addEventListener("wheel", (event) => {
-    if (scrollDelay.current) {
-      return;
+  useEffect(() => {
+    const scrollEvent = addEventListener("wheel", (e) => scroll(e));
+    function scroll(event) {
+      if (scrollDelay.current) {
+        return;
+      }
+      scrollDelay.current = true;
+      if (event.deltaY < 0 && scrollRef.current > 0) {
+        setScrollPoint((previous) => previous - 1);
+      } else if (
+        event.deltaY > 0 &&
+        scrollRef.current < typeHolder.length - 1
+      ) {
+        setScrollPoint((previous) => previous + 1);
+      }
+      setTimeout(() => {
+        scrollDelay.current = false;
+      }, 500);
     }
-    let diff;
-    if (event.deltaY < 0) {
-      diff = -1;
-    } else if (event.deltaY > 0) {
-      diff = 1;
-    }
-    scrollDelay.current = true;
-    setScrollPoint((previous) => previous + diff);
-    setTimeout(() => {
-      scrollDelay.current = false;
-    }, 500);
-  });
-  const contentHolder = {
-    about: [
-      <p key="about-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>,
-    ],
-    project1: [
-      <p key="project1-1">
-        Run tracker ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
-      </p>,
-      <p key="project1-2">adjfkn juvi rekjsrenf</p>,
-    ],
-    project2: [
-      <p key="project2-1">
-        Spreadsheet creep Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </p>,
-      <p key="project2-2">sdjf ubs ifwenfki sdkjf</p>,
-    ],
-    contact: [
-      <p key="contact-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>,
-    ],
-  };
+    removeEventListener("wheel", scrollEvent);
+  }, []);
+  const typeHolder = [
+    {
+      id: 0,
+      type: "about",
+      content: [
+        <p key="about-1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>,
+      ],
+    },
+    {
+      id: 1,
+      type: "project1",
+      content: [
+        <p key="project1-1">
+          Run tracker ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </p>,
+        <p key="project1-2">adjfkn juvi rekjsrenf</p>,
+      ],
+    },
+    {
+      id: 2,
+      type: "project2",
+      content: [
+        <p key="project2-1">
+          Spreadsheet creep Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua.
+        </p>,
+        <p key="project2-2">sdjf ubs ifwenfki sdkjf</p>,
+      ],
+    },
+    {
+      id: 3,
+      type: "contact",
+      content: [
+        <p key="contact-1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>,
+      ],
+    },
+  ];
 
-  function Tab({ title, renderTitle, subTitles }) {
+  function Tab({ type, renderTitle, subTitles }) {
+    const currentType = typeHolder[scrollPoint].type;
     if (!subTitles) {
       return (
-        <div className={tabOrder[scrollPoint] === title ? "activeTab" : "tab"}>
-          <Title title={title} renderTitle={renderTitle} />
-          {tabOrder[scrollPoint] === title ? <Content /> : null}
+        <div className={currentType === type ? "activeTab" : "tab"}>
+          <Title type={type} renderTitle={renderTitle} />
+          {currentType === type ? <Content /> : null}
         </div>
       );
     }
@@ -63,32 +87,36 @@ export default function App() {
       return (
         <div
           className={
-            tabOrder[scrollPoint] === subTitles[0].title ||
-            tabOrder[scrollPoint] === subTitles[1].title
+            currentType === subTitles[0].type ||
+            currentType === subTitles[1].type
               ? "activeTab"
               : "tab"
           }
         >
-          {tabOrder[scrollPoint] === subTitles[0].title ||
-          tabOrder[scrollPoint] === subTitles[1].title ? (
+          {currentType === subTitles[0].type ||
+          currentType === subTitles[1].type ? (
             <>
               <SubTitles subTitles={subTitles} renderTitle={renderTitle} />
               <Content />
             </>
           ) : (
-            <Title title={title} renderTitle={renderTitle} />
+            <Title type={type} renderTitle={renderTitle} />
           )}
         </div>
       );
-      function Title({ title, renderTitle }) {
+      function Title({ type, renderTitle }) {
         let className = "title";
-        if (tabOrder[scrollPoint] !== title) {
+        if (currentType !== type) {
           className += " inactiveTitle";
         }
         return (
           <p
             className={className}
-            onClick={() => setScrollPoint(tabOrder.indexOf(title))}
+            onClick={() =>
+              setScrollPoint(
+                typeHolder.find((object) => object.type === type).id
+              )
+            }
           >
             {renderTitle}
           </p>
@@ -96,9 +124,7 @@ export default function App() {
       }
       function Content() {
         return (
-          <div className="contentHolder">
-            {contentHolder[tabOrder[scrollPoint]]}
-          </div>
+          <div className="contentHolder">{typeHolder[scrollPoint].content}</div>
         );
       }
       function SubTitles({ subTitles, renderTitle }) {
@@ -108,7 +134,7 @@ export default function App() {
               className="title"
               style={{
                 gridColumn: "1/2",
-                gridRow: Number(tabOrder[scrollPoint].slice(-1)) + "/3",
+                gridRow: Number(currentType.slice(-1)) + "/3",
               }}
             >
               {renderTitle} -
@@ -120,27 +146,31 @@ export default function App() {
           subTitles.forEach((subTitle) => {
             holder.push(
               <SubTitle
-                key={subTitle.title + "key"}
-                title={subTitle.title}
+                key={subTitle.type + "key"}
+                type={subTitle.type}
                 renderTitle={subTitle.renderTitle}
               />
             );
           });
-          function SubTitle({ title, renderTitle }) {
+          function SubTitle({ type, renderTitle }) {
             let style = {
               gridColumn: "2/3",
               gridRow:
-                Number(title.slice(-1)) + "/" + (Number(title.slice(-1)) + 1),
+                Number(type.slice(-1)) + "/" + (Number(type.slice(-1)) + 1),
             };
             let className = "title";
-            if (tabOrder[scrollPoint] !== title) {
+            if (currentType !== type) {
               className += " inactiveTitle";
             }
             return (
               <p
                 className={className}
                 style={style}
-                onClick={() => setScrollPoint(tabOrder.indexOf(title))}
+                onClick={() =>
+                  setScrollPoint(
+                    typeHolder.find((object) => object.type === type).id
+                  )
+                }
               >
                 {renderTitle}
               </p>
@@ -156,15 +186,17 @@ export default function App() {
         );
       }
     }
-    function Title({ title, renderTitle }) {
+    function Title({ type, renderTitle }) {
       let className = "title";
-      if (tabOrder[scrollPoint] !== title) {
+      if (currentType !== type) {
         className += " inactiveTitle";
       }
       return (
         <p
           className={className}
-          onClick={() => setScrollPoint(tabOrder.indexOf(title))}
+          onClick={() =>
+            setScrollPoint(typeHolder.find((object) => object.type === type).id)
+          }
         >
           {renderTitle}
         </p>
@@ -172,25 +204,23 @@ export default function App() {
     }
     function Content() {
       return (
-        <div className="contentHolder">
-          {contentHolder[tabOrder[scrollPoint]]}
-        </div>
+        <div className="contentHolder">{typeHolder[scrollPoint].content}</div>
       );
     }
   }
   return (
     <>
       <p className="title">Thomas Evans</p>
-      <Tab title="about" renderTitle="About" />
+      <Tab type="about" renderTitle="About" />
       <Tab
-        title="project1"
+        type="project1"
         renderTitle="Projects"
         subTitles={[
-          { title: "project1", renderTitle: "Run Tracker" },
-          { title: "project2", renderTitle: "Spreadsheet Creep" },
+          { type: "project1", renderTitle: "Run Tracker" },
+          { type: "project2", renderTitle: "Spreadsheet Creep" },
         ]}
       />
-      <Tab title="contact" renderTitle="Contact" />
+      <Tab type="contact" renderTitle="Contact" />
     </>
   );
 }
