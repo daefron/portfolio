6b4a69501db71d854991ce9db7e21049cc67308d
Daefron <thomas_evans@outlook.com>
2024-12-27 20:21:51 +1100

    scrolling perfected, copy added, images added

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,10 @@ export default function App() {
   const scrollDelay = useRef(false);
   useEffect(() => {
     const scrollEvent = addEventListener("wheel", (e) => scroll(e));
-    const touchStartEvent = addEventListener("touchstart", touchStart);
     const touchEndEvent = addEventListener("touchend", touchEnd);
     const touchMoveEvent = addEventListener("touchmove", (e) => touchMove(e));
     let lastTouch;
     function touchEnd() {
-      removeEventListener("touchstart", touchStartEvent);
       removeEventListener("touchend", touchEndEvent);
       removeEventListener("touchmove", touchMoveEvent);
       lastTouch = undefined;
@@ -27,7 +25,6 @@ export default function App() {
           touchEnd();
           return;
           function touchScroll(distance) {
-            console.log(distance);
             if (scrollDelay.current) {
               return;
             }
@@ -44,13 +41,12 @@ export default function App() {
             }
             setTimeout(() => {
               scrollDelay.current = false;
-            }, 750);
+            }, 800);
           }
         }
       }
       lastTouch = touch;
     }
-    function touchStart() {}
     function scroll(event) {
       if (scrollDelay.current) {
         return;
@@ -68,7 +64,7 @@ export default function App() {
       }
       setTimeout(() => {
         scrollDelay.current = false;
-      }, 750);
+      }, 800);
     }
     removeEventListener("wheel", scrollEvent);
   }, []);
@@ -78,10 +74,32 @@ export default function App() {
       type: "about",
       content: [
         <p key="about-1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          I am a completely self-taught front-end web developer.
+        </p>,
+        <p key="about-2">
+          Over the years, I have tried to get into programming who knows how
+          many times (I even started studying it at uni briefly) but I never
+          could stick with it. Last year, I started working through The Odin
+          Project.
+        </p>,
+        <p key="about-3">It stuck.</p>,
+        <p key="about-4" className="subTitle">
+          Skills:
+        </p>,
+        <p key="about-5">
+          At this stage, I would say that I am extremely comfortable with the
+          foundations (HTML, CSS, JS) of front-end web development (enough so
+          that I believe I could make pretty much anything I put my mind to),
+          comfortable with React, and have just started dipping my toes into the
+          back-end.
+        </p>,
+        <p key="about-6" className="subTitle">
+          This portfolio:
+        </p>,
+        <p key="about-7">
+          While The Odin Project did get me to make a variety of basic projects,
+          I decided not to include them in this portfolio and only showcase my
+          self-driven projects. You probably already know what the weather is...
         </p>,
       ],
     },
@@ -90,10 +108,35 @@ export default function App() {
       type: "project1",
       content: [
         <p key="project1-1">
-          Run tracker ut enim ad minim veniam, quis nostrud exercitation ullamco
-          laboris nisi ut aliquip ex ea commodo consequat.
+          A custom built dashboard that shows run data gathered from the Fitbit
+          API in a variety of graphs and lists.
         </p>,
-        <p key="project1-2">adjfkn juvi rekjsrenf</p>,
+        <img key="project1-2" src="./docs/images/runPage.png" />,
+        <p key="project1-3">
+          I had just started getting into running, and I found that Fitbit's
+          data visualisation didn't allow for direct comparison of exercises (+
+          I felt I needed more experience with APIs and wanted to learn a chart
+          library) so I decided to make a dashboard for tracking my runs.
+        </p>,
+        <p key="project1-4" className="subTitle">
+          Built with:
+        </p>,
+        <div style={{ display: "flex", gap: "10px" }} key="project1-5">
+          <a href="https://react.dev/">React</a>
+          <a href="https://recharts.org/">Recharts</a>
+          <a href="https://www.fitbit.com/dev">Fitbit API</a>
+        </div>,
+        <p key="project1-6" className="subTitle">
+          Links:
+        </p>,
+        <div key="project1-7" style={{ display: "flex" }}>
+          <p>Live site link: &nbsp;</p>
+          <a href="https://runtracker.netlify.app">Here</a>
+        </div>,
+        <div key="project1-8" style={{ display: "flex" }}>
+          <p>Github link: &nbsp;</p>
+          <a href="https://github.com/daefron/run-tracker">Here</a>
+        </div>,
       ],
     },
     {
@@ -101,21 +144,51 @@ export default function App() {
       type: "project2",
       content: [
         <p key="project2-1">
-          Spreadsheet creep Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua.
+          A "game engine" set within the bounds of a spreadsheet.
         </p>,
-        <p key="project2-2">sdjf ubs ifwenfki sdkjf</p>,
+        <img key="project2-2" src="./docs/images/spreadsheetPage.png" />,
+        <p key="project2-3">
+          I needed an interesting, first project to make in React, and the first
+          thing that came to mind (when I was having a shower of course) was a
+          tower defense game set in a spreadsheet.
+        </p>,
+        <p key="project2-4">
+          At this stage, it's more of an engine than a playable game, but for
+          what it is I am proud of it.
+        </p>,
+        <p key="project2-5" className="subTitle">
+          Built with:
+        </p>,
+        <div style={{ display: "flex", gap: "10px" }} key="project2-6">
+          <a href="https://react.dev/">React</a>
+        </div>,
+        <p key="project2-7" className="subTitle">
+          Links:
+        </p>,
+        <div key="project2-8" style={{ display: "flex" }}>
+          <p>Live site link: &nbsp;</p>
+          <a href="https://spreadsheetcreep.netlify.app">Here</a>
+        </div>,
+        <div key="project2-9" style={{ display: "flex" }}>
+          <p>Github link: &nbsp;</p>
+          <a href="https://github.com/daefron/spreadsheet-creep">Here</a>
+        </div>,
       ],
     },
     {
       id: 3,
       type: "contact",
       content: [
-        <p key="contact-1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>,
+        <div key="contact-1" style={{ display: "flex" }}>
+          <p className="subTitle">Email: &nbsp;</p>
+          <p className="subTitle">thomas_evans@outlook.com</p>
+        </div>,
+        <div key="contact-2" style={{ display: "flex" }}>
+          <p className="subTitle">Github: &nbsp;</p>
+          <a className="subTitle" href="https://github.com/daefron">
+            daefron
+          </a>
+        </div>,
       ],
     },
   ];
@@ -169,7 +242,7 @@ export default function App() {
       }
       return (
         <animated.div
-          className={selected ? "tab activeTab" : "tab"}
+          className="tab"
           style={
             selected ? growAnimation : lastSelected ? shrinkAnimation : null
           }
@@ -194,7 +267,7 @@ export default function App() {
       let visible;
       if (subTitleTypeIsCurrent && subTitleTypeIsLast) {
         animation = bothAnimation;
-        visible = true;
+        visible = "both";
       } else if (subTitleTypeIsCurrent) {
         animation = growAnimation;
         visible = true;
@@ -202,11 +275,52 @@ export default function App() {
         animation = shrinkAnimation;
         visible = "last";
       }
+      if (visible === "both") {
+        if (typeHolder[scrollPoint + 1].type !== subTitleTypeIsLast.type) {
+          animation = useSpring({
+            from: { flexGrow: 1, right: "0px" },
+            to: { flexGrow: 1, right: "calc(100% + 30px)" },
+            config: { duration: 0 },
+          });
+          return (
+            <div className="tab" style={{ flexGrow: 1 }}>
+              <SubTitles subTitles={subTitles} renderTitle={renderTitle} />
+              <animated.div className="subContentHolder" style={animation}>
+                <StaticContent visible={"last"} />
+                <StaticContent visible={visible} />
+              </animated.div>
+            </div>
+          );
+        } else {
+          animation = useSpring({
+            from: { flexGrow: 1, right: "100%" },
+            to: { flexGrow: 1, right: "0px" },
+            config: { duration: 0 },
+          });
+          return (
+            <div className="tab" style={{ flexGrow: 1 }}>
+              <SubTitles subTitles={subTitles} renderTitle={renderTitle} />
+              <animated.div className="subContentHolder" style={animation}>
+                <StaticContent visible={visible} />
+                <StaticContent visible={"last"} />
+              </animated.div>
+            </div>
+          );
+        }
+        function StaticContent({ visible }) {
+          return (
+            <div className="contentHolder">
+              {visible === "last"
+                ? typeHolder[lastScroll.current].content
+                : visible
+                ? typeHolder[scrollPoint].content
+                : null}
+            </div>
+          );
+        }
+      }
       return (
-        <animated.div
-          className={selected ? "tab activeTab" : "tab"}
-          style={animation}
-        >
+        <animated.div className="tab" style={animation}>
           <SubTitles subTitles={subTitles} renderTitle={renderTitle} />
           <Content visible={visible} />
         </animated.div>
@@ -247,6 +361,16 @@ export default function App() {
                     setScrollPoint(
                       typeHolder.find((object) => object.type === type).id
                     );
+                  }}
+                  onMouseOver={(e) => {
+                    if (e.target.className.includes("inactive")) {
+                      e.target.style.color = "rgb(190, 190, 190)";
+                    }
+                  }}
+                  onMouseOut={(e) => {
+                    if (e.target.className.includes("inactive")) {
+                      e.target.style.color = "rgb(150, 150, 150)";
+                    }
                   }}
                 >
                   {renderTitle}
@@ -289,6 +413,16 @@ export default function App() {
                     typeHolder.find((object) => object.type === type).id
                   );
                 }}
+                onMouseOver={(e) => {
+                  if (e.target.className.includes("inactive")) {
+                    e.target.style.color = "rgb(190, 190, 190)";
+                  }
+                }}
+                onMouseOut={(e) => {
+                  if (e.target.className.includes("inactive")) {
+                    e.target.style.color = "rgb(150, 150, 150)";
+                  }
+                }}
               >
                 {renderTitle}
               </p>
@@ -314,6 +448,16 @@ export default function App() {
       return (
         <p
           className={className}
+          onMouseOver={(e) => {
+            if (e.target.className.includes("inactive")) {
+              e.target.style.color = "rgb(190, 190, 190)";
+            }
+          }}
+          onMouseOut={(e) => {
+            if (e.target.className.includes("inactive")) {
+              e.target.style.color = "rgb(150, 150, 150)";
+            }
+          }}
           onClick={() => {
             lastScroll.current = scrollRef.current;
             setScrollPoint(
